b150d7b003034553e602694b2ab7bda58d224e01
datnguyen <datnguyen661995@gmail.com>
2023-11-01 11:52:16 +0700

    add code

---
+++ src/main/java/com/datnguyen/jenkin/java/JenkinJavaApplication.java
@@ -0,0 +1,13 @@
+package com.datnguyen.jenkin.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datnguyen/jenkin/java/JenkinJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datnguyen.jenkin.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
